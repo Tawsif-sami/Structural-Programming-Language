fe35bb0af683bee1795b69de5353cb05696e2ebf
Tawsif-sami <nobletawsif@gmail.com>
2026-09-08 12:45:04 +0600

    Add files via upload

---
+++ c variable.cxx
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int studentid = 15;
+    int studentage = 23;
+    float studentfee = 70.45;
+    char studentgrade = 'b';
+
+    printf("students id : %d\n", studentid);
+    printf("students age : %d\n", studentage);
+    printf("students fee : %g\n", studentfee);
+    printf("students grade : %d\n", studentgrade);
+
+    return 0;
+}
