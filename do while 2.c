62446beecf2cb5d738f1617054b2e1ed6d260316
Tawsif-sami <nobletawsif@gmail.com>
2026-09-13 10:20:49 +0600

    Add files via upload

---
+++ do while 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i,n=10;
+	int password, name;
+	printf("Enter value for n : \0\0");
+/*	printf("Enter your name & password : \n");
+	scanf("%d",&password);
+	gets(name); */
+	do{
+	    printf("%d\n",n);
+	    n--;
+	}while(n>0);
+return 0;
+}
+
